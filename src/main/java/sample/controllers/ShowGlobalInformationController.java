aa25a2a9d253cb8df2485394b02d2cca3d3d0887
janosh <jano.ssh@gmail.com>
2017-12-06 23:20:18 +0200

    06122017 commit

--- src/main/java/sample/controllers/ShowGlobalInformationController.java
+++ src/main/java/sample/controllers/ShowGlobalInformationController.java
@@ -20,12 +20,16 @@ public class ShowGlobalInformationController {
 
     @FXML
     private TextArea resultString;
+    @FXML
+    private TextArea resultString2;
 
 
     @FXML
     public void buttonCanselAction() {
         channelIdsString.setText("");
         resultString.setText("");
+        resultString2.setVisible(false);
+        resultString2.setText("");
     }
 
     @FXML
@@ -35,14 +39,20 @@ public class ShowGlobalInformationController {
         ArrayList<String> ids = new ArrayList<String>();
         for (String id : channelIds) {
             if (id != null && !id.isEmpty())
-                ids.add(id);
+                ids.add(id.trim());
         }
-        if (ids.size() == 1) {
+        if (ids.size() == 1 || ids.size() == 2) {
             ArrayList<ChannelResponse> channelResponses = new ArrayList<ChannelResponse>();
+            String idsString = "";
+            for (int i = 0; i < ids.size(); i++) {
+                idsString += ids.get(i) + (i != ids.size() - 1 ? "," : "");
+            }
+
+
             Date start = new Date();
             try {
 
-                ArrayList<ChannelResponse> items = ChannelRequest.getChannelResponse(channelIdsString.getText()).getItems();
+                ArrayList<ChannelResponse> items = ChannelRequest.getChannelResponse(idsString).getItems();
 
                 for (ChannelResponse request : items) {
 
@@ -55,21 +65,40 @@ public class ShowGlobalInformationController {
                 e.printStackTrace();
             }
             Date end = new Date();
-            if (channelResponses.size() == 1) {
+            if (channelResponses.size() == 1 || channelResponses.size() == 2) {
 
-                String result = //channelResponses.get(0).getId() + System.lineSeparator() +
-                                "1. " + channelResponses.get(0).getSnippet().getTitle() + System.lineSeparator() +
-                                "2. " + channelResponses.get(0).getSnippet().getPublishedAt() + System.lineSeparator() +
-                                "3. " + channelResponses.get(0).getStatistics().getSubscriberCount() + System.lineSeparator() +
-                                "4. " + channelResponses.get(0).getStatistics().getVideoCount() + System.lineSeparator() +
-                                "5. " + channelResponses.get(0).getStatistics().getViewCount();
-                if(Settings.isShowLeadTime()){
-                    result += System.lineSeparator() + "6. LeadTime -- "+((double)(end.getTime() - start.getTime())/1000)+ " second(s)";
+                String result = perfomResult(channelResponses, 0);
+                //channelResponses.get(0).getId() + System.lineSeparator() +
+
+                if (Settings.isShowLeadTime()) {
+                    result += System.lineSeparator() + "6. LeadTime -- " + ((double) (end.getTime() - start.getTime()) / 1000) + " second(s)";
                 }
                 resultString.setText(result);
+                resultString2.setVisible(false);
+            }
+
+            if (channelResponses.size() == 2) {
+
+                String result = perfomResult(channelResponses, 1);
+                //channelResponses.get(0).getId() + System.lineSeparator() +
+
+                if (Settings.isShowLeadTime()) {
+                    result += System.lineSeparator() + "6. LeadTime -- " + ((double) (end.getTime() - start.getTime()) / 1000) + " second(s)";
+                }
+                resultString2.setText(result);
+                resultString2.setVisible(true);
             }
 
         }
 
     }
+
+    private String perfomResult(ArrayList<ChannelResponse> channelResponses, int index) {
+        return "1. " + channelResponses.get(index).getSnippet().getTitle() + System.lineSeparator() +
+                "2. " + channelResponses.get(index).getSnippet().getPublishedAt() + System.lineSeparator() +
+                "3. " + channelResponses.get(index).getStatistics().getSubscriberCount() + System.lineSeparator() +
+                "4. " + channelResponses.get(index).getStatistics().getVideoCount() + System.lineSeparator() +
+                "5. " + channelResponses.get(index).getStatistics().getViewCount();
+    }
+
 }
